e84f797939d17693e7b78ec0da8b2001e5811ff8
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-09 16:01:01 -0300

    Update app.py

--- codigo-fonte/app.py
+++ codigo-fonte/app.py
@@ -1,10 +1,13 @@
+import os
+
 class Conta:
     def __init__(self,numero_conta,titular,saldo,limite):
         self.__numero_conta = numero_conta
         self.__titular = titular
         self.__saldo = saldo
         self.__limite = limite
-    
+        self.agencia = '0001'
+
     def get_numero_conta(self):
         return f'{self.__numero_conta}'
     
@@ -15,7 +18,7 @@ class Conta:
         return f'R${self.__saldo}'
     
     def get_limite(self):
-        return f'{self.__limite}'
+        return f'R${self.__limite}'
     
     
     def depositar(self,valor):
@@ -29,19 +32,27 @@ class Conta:
         else:
             return f'{self.get_titular()},não foi possivel efetuar o saque pelo motivo: Saldo Insuficiente'
 
-    def transferir(self,valor,conta_destino):
-        if self.sacar(valor):
-            conta_destino.depositar(valor)
+    def transferir(self,valor):
+        if self.__saldo >= valor:
+            self.sacar(valor)
             print(f'Transferencia de R${valor} realizada com sucesso!')
         else:
             print(f'Transferencia de R${valor} não realizada, Saldo insuficiente.')
+
+    def exibir_detalhes(self):
+        os.system('cls')
+        return f'Detalhes da conta {self.get_numero_conta()}\n---------------------------------\nNome: {self.get_titular()}\nAgencia: {self.agencia}\nSaldo: {self.get_saldo()}\nLimite Cheque Especial: {self.get_limite()}'
 
 
 
 
 pessoa1 = Conta(22234,'Giovanni Finetto',1000,200)
-print(f'Olá {pessoa1.get_titular()} seu saldo atual é de {pessoa1.get_saldo()}')
-saque1 = pessoa1.sacar(950)
+detalhes = pessoa1.exibir_detalhes()
+print(detalhes)
+
+pessoa1.transferir(1000)
+print(f'\nO seu saldo agora é de {pessoa1.get_saldo()}')
+
 print(f'{pessoa1.get_titular()}, {saque1}')
 saque2 = pessoa1.sacar(950)
 print(f'{saque2}')

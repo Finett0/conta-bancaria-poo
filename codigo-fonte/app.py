a94a8d6036eec0a60e2e44cf58fb2dbc7185b7cb
Giovanni Finetto <118924567+Finett0@users.noreply.github.com>
2025-01-09 15:42:24 -0300

    Update app.py

--- codigo-fonte/app.py
+++ codigo-fonte/app.py
@@ -12,16 +12,36 @@ class Conta:
         return f'{self.__titular}'
     
     def get_saldo(self):
-        return f'{self.__saldo}'
+        return f'R${self.__saldo}'
     
     def get_limite(self):
         return f'{self.__limite}'
+    
     
     def depositar(self,valor):
         if valor > 0:
             self.__saldo += valor
 
     def sacar(self,valor):
-        if valor > 0 and self.__saldo > 0:
+        if self.__saldo >= valor:
             self.__saldo -= valor
-            return valor
+            return f'O saque de R${valor} foi feito com sucesso seu saldo agora é de {self.get_saldo()}'
+        else:
+            return f'{self.get_titular()},não foi possivel efetuar o saque pelo motivo: Saldo Insuficiente'
+
+    def transferir(self,valor,conta_destino):
+        if self.sacar(valor):
+            conta_destino.depositar(valor)
+            print(f'Transferencia de R${valor} realizada com sucesso!')
+        else:
+            print(f'Transferencia de R${valor} não realizada, Saldo insuficiente.')
+
+
+
+
+pessoa1 = Conta(22234,'Giovanni Finetto',1000,200)
+print(f'Olá {pessoa1.get_titular()} seu saldo atual é de {pessoa1.get_saldo()}')
+saque1 = pessoa1.sacar(950)
+print(f'{pessoa1.get_titular()}, {saque1}')
+saque2 = pessoa1.sacar(950)
+print(f'{saque2}')
